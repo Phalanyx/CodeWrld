0ac753f74517f53d0a8d3d47eb1dcb63f9aa4ad6
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2024-11-27 15:25:23 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user : 'ubuntu',
       host : '18.227.209.209',
       ref  : 'origin/master',
-      repo : 'https://github.com/Phalanyx/Scriptorium.git',
+      repo : 'git@github.com:Phalanyx/Scriptorium.git',
       path : '/home/ubuntu',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/.nvm/nvm.sh && sh startup.sh && npm run build && pm2 reload ecosystem.config.js --env production',
